dcad28e80961bc232e5648df77c547f2774e9305
GachaLifeNPC <gachalifenpc@gmail.com>
2024-09-20 18:30:24 +0700

    Update player.js

--- chunks/player.js
+++ chunks/player.js
@@ -107,6 +107,7 @@ var Player = function Player(_ref) {
   }, isPlayerOnly && /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("button", {
     onClick: onSeeInside
   }, 'See inside'), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(_containers_gui_jsx__WEBPACK_IMPORTED_MODULE_7__["default"], {
+    accountNavOpen: true,
     canSave: true,
     canShare: true,
     canRemix: true,
